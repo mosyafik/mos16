d6301ecee00f58dba0ee840c48a1c8495e165715
Mosyafik Jr <63044745+mosyafik@users.noreply.github.com>
2022-09-24 13:08:21 +0700

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -534,14 +534,14 @@ const template = generateWAMessageFromContent(m.key.remoteJid, proto.Message.fro
 handler.help = ['menu', 'help', '?']
 handler.tags = ['main']
 handler.command = /^(menu|help|\?)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
+handler.owner = true
+handler.mods = true
+handler.premium = true
+handler.group = true
+handler.private = true
 
-handler.admin = false
-handler.botAdmin = false
+handler.admin = true
+handler.botAdmin = true
 
 handler.fail = null
 handler.exp = 3
